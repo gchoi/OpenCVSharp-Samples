a9a687570ce411d1edf7ac1375e3a713ce6b4277
NATICK-NT\achoi <cinema4dr12@gmail.com>
2018-08-15 06:57:13 +0700

    TEMP

---
+++ ImageFlipping/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVSharpSample02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var src = new Mat(@"D:\Projects\COGNEX\SDC-LaserCut\Samples\20180601\BPL_CUT_Normal\OK_A3NE1Q7CVC1AP020_R2_2018-05-15_22-12-28-2211.jpg", ImreadModes.Unchanged);
+            var flipped = src.Flip(0);
+
+            using (var window = new Window("window", image: flipped, flags: WindowMode.AutoSize))
+            {
+                Cv2.WaitKey();
+            }
+        }
+
+    }
+}
